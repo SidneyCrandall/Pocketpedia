aed749c4e367fb887f626c221c9f9368a5a9eb67
Sidney Crandall <sdncrandall5@gmail.com>
2021-07-23 15:48:36 -0500

    Create SQL folder

--- 02_Pocketpedia_Seed_Data.sql
+++
@@ -1,2 +0,0 @@
-﻿INSERT INTO UserProfile (Id, FirebaseId, Email, DisplayName, IslandName, IslandPhrase) 
-VALUES (1, 'WZ8zeLK1kIhe5GDydzhh8PSiOz02', 'sc@sc.com', 'Squidney', 'Gallifrey', 'Down and Out Island Dweller');
