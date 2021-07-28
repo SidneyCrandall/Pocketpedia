76e5fbe74f6267b525aef7add75adf764b277fcb
Sidney Crandall <sdncrandall5@gmail.com>
2021-07-28 16:04:27 -0500

    Added LocationRepo to be called throughout

---
+++ Pocketpedia/Repositories/ILocationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Pocketpedia.Models;
+using System.Collections.Generic;
+
+namespace Pocketpedia.Repositories
+{
+    public interface ILocationRepository
+    {
+       public List<Location> GetLocations();
+    }
+}
---
+++ Pocketpedia/Repositories/LocationRepository.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using Pocketpedia.Models;
+using Pocketpedia.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pocketpedia.Repositories
+{
+    public class LocationRepository : BaseRepository, ILocationRepository
+    {
+        public LocationRepository(IConfiguration config) : base(config) { }
+
+        public List<Location> GetLocations()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT l.Id,
+                                               l.Name
+                                        FROM Location l";
+
+                    var reader = cmd.ExecuteReader();
+
+                    var locations = new List<Location>();
+
+                    while (reader.Read())
+                    {
+                        locations.Add(new Location()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            Name = DbUtils.GetString(reader, "Name")
+                        });
+                    }
+
+                    reader.Close();
+
+                    return locations;
+                }
+            }
+        }
+
+        public Location GetById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT l.Id as LocationId,
+                                               l.Name
+                                        FROM Location l
+                                        WHERE l.Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    var reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        Location location = new Location()
+                        {
+                            Id = DbUtils.GetInt(reader, "LocationId"),
+                            Name = DbUtils.GetString(reader, "Name")
+                        };
+
+                        reader.Close();
+
+                        return location;
+                    }
+
+                    reader.Close();
+
+                    return null;
+                }
+            }
+        }
+        
+    }
+}
